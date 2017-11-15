30bf09f2f6902b2bc44b86e8b4d165467faefd70
Laura Pomerleau <llpomerl@us.ibm.com>
2017-11-15 12:46:20 -0500

    update header/footer constant

--- src/app/Constants.ts
+++ src/app/Constants.ts
@@ -21,10 +21,8 @@ export class Constants {
   static readonly apiUrl = `${Constants.PROTOCOL}//${Constants['DOMAIN_NAME']}/api/${Constants['CONTENT_HUB_ID']}`;
   static readonly deliveryUrl = `${Constants.PROTOCOL}//${Constants['DOMAIN_NAME']}/${Constants['CONTENT_HUB_ID']}`;
 
-  static readonly FOOTER_CONFIG = 'footerConfig';
-  static readonly HEADER_CONFIG = 'headerConfig';
-
-
+  static readonly FOOTER_CONFIG = 'eshopperFooterConfig';
+  static readonly HEADER_CONFIG = 'eshopperHeaderConfig';
 
   /* Sort order */
   static readonly ALPHABETICAL_ASCENDING = 'Alphabetical ascending';
@@ -41,19 +39,9 @@ export class Constants {
   static readonly LAST_30_DAYS = 'Published last 30 days';
   static readonly LAST_7_DAYS = 'Published last 7 days';
 
-  /* TODO remove */
-  /* Dynamic list types */
-  static readonly DESIGN_ARTICLES = 'Design article';
-  static readonly DESIGNERS = 'Designers';
-  static readonly EVENTS = 'Event';
-
-
   /* Sort field type */
   static readonly DATETIME = 'datetime';
 
   /* The page that sends web message to SPA's iframe */
   static readonly SITE_MANAGER_URL = 'http://localhost:8000';
-
 }
-
-
