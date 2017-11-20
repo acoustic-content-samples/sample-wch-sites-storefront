d6f008fa2a70963fd4b0449f74c64c4d8a005f92
Laura Pomerleau <llpomerl@us.ibm.com>
2017-11-20 13:46:38 -0500

    first pull of site

---
+++ content-artifacts/assets/assets/js/gmaps.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Error</title>
+</head>
+<body>
+<pre>Cannot GET /api/93ad2da4-9dc6-4290-9ef5-0f8d3d087748/authoring/v1/resources/75a1b69b80f43aaaf74e3f56ca80e59d</pre>
+</body>
+</html>
---
+++ content-artifacts/assets/assets/js/price-range.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Error</title>
+</head>
+<body>
+<pre>Cannot GET /api/93ad2da4-9dc6-4290-9ef5-0f8d3d087748/authoring/v1/resources/3767771a6c243c93b9c1304a05cf800e</pre>
+</body>
+</html>
