a2351ed832f3324120fa8c015afe3ce0c98db81a
Laura Pomerleau <llpomerl@us.ibm.com>
2017-11-29 17:48:44 -0500

    login link changes to logout, when logged in

--- src/app/wchHeader/wchHeader.component.ts
+++ src/app/wchHeader/wchHeader.component.ts
@@ -16,6 +16,7 @@
 import {
 	Component,
 	Input,
+	OnInit,
 	OnDestroy,
 	ViewEncapsulation
 } from '@angular/core';
@@ -24,18 +25,23 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 
-import {LayoutComponent, RenderingContext, AbstractRenderingComponent} from "ibm-wch-sdk-ng";
-import {ConfigServiceService} from "../common/configService/config-service.service";
-import {Constants} from "../Constants";
-import {Subscription} from "rxjs/Subscription";
+import { Subscription } from "rxjs/Subscription";
+import { Constants } from "../Constants";
+import { LayoutComponent, RenderingContext, AbstractRenderingComponent } from "ibm-wch-sdk-ng";
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ConfigServiceService } from "../common/configService/config-service.service";
+import { AuthenticationTransactionService } from '../commerce/services/componentTransaction/authentication.transaction.service';
 
 @Component({
 	selector: 'wch-header',
 	templateUrl: './wch-header.html',
 	encapsulation: ViewEncapsulation.None
 })
-export class WchHeaderComponent implements OnDestroy {
+export class WchHeaderComponent implements OnInit, OnDestroy {
 
+	isLoggedIn: boolean = false;
+	authSub: any;
 	rc: RenderingContext;
 	headerConfig: any;
 	configSub: Subscription;
@@ -55,14 +61,33 @@ export class WchHeaderComponent implements OnDestroy {
 		this.rc = aValue;
 	}
 
-	constructor(configService: ConfigServiceService) {
+	constructor(configService: ConfigServiceService, private router: Router, private authService: AuthenticationTransactionService) {
 		this.configSub = configService.getConfig(Constants.HEADER_CONFIG).subscribe((context) => {
 			this.headerConfig = context;
 		});
 	}
+ 
+	ngOnInit() {
+		this.isLoggedIn = this.authService.isLoggedIn();
+		// subscribe to the observable authentication status
+		this.authSub = this.authService.authUpdate.subscribe(status => this.isLoggedIn = status);
+	}
 
 	ngOnDestroy() {
 		this.configSub.unsubscribe();
+		this.authSub.unsubscribe();
+	}
+
+	private logout() {
+		this.authService.logout().then(res => {
+			// navigate home after a successful logout
+			this.router.navigate(['/home']);
+		}).catch(e=>this.handleError(e,"Unable to logout successfully"));
+	}
+
+	private handleError(error:HttpErrorResponse,fallback:string) {
+		const eBody=error.error;
+		return eBody.errors&&eBody.errors.length&&eBody.errors[0].errorMessage?eBody.errors[0].errorMessage:fallback;
 	}
 
 	isElementAvailable(elem): boolean {
